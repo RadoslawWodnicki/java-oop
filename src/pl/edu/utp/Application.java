e32b62cb3742cbe9fcffdb3cd1d81ee1d149cdb7
RadoslawWodnicki <radwod002@utp.edu.pl>
2021-03-18 18:29:54 +0100

    First repo code line

---
+++ src/pl/edu/utp/Application.java
@@ -0,0 +1,12 @@
+package pl.edu.utp;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	System.out.println("My first project from remote repo!");
+		
+	}
+
+}
